8ed9c081ac8e216774528c70403071dfe10a58bd
kvoli <amcclernon@student.unimelb.edu.au>
2021-08-26 02:28:36 +1000

    add

--- conf/lsp.lua
+++ conf/lsp.lua
@@ -147,4 +147,5 @@ cmp.setup {
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
+require('rust-tools').setup({})
 vim.lsp.set_log_level("debug")
