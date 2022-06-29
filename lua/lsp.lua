b3cb9a596a87ed114d4c81504d02ef6a99da3173
kvoli <austen.mcclernon@gmail.com>
2022-06-28 20:27:38 -0400

    update dapui config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -441,7 +441,6 @@ require("telescope").load_extension('harpoon')
 require("telescope").load_extension('goimpl')
 
 require('neogen').setup { enabled = true }
-
 require("dapui").setup({
   icons = { expanded = "▾", collapsed = "▸" },
   mappings = {
@@ -451,31 +450,51 @@ require("dapui").setup({
     remove = "d",
     edit = "e",
     repl = "r",
+    toggle = "t",
   },
-  sidebar = {
-    -- You can change the order of elements in the sidebar
-    elements = {
-      -- Provide as ID strings or tables with "id" and "size" keys
-      { id = "watches", size = 0.3 },
-      {
-        id = "scopes",
-        size = 0.3, -- Can be float or integer > 1
+  -- Expand lines larger than the window
+  -- Requires >= 0.7
+  expand_lines = vim.fn.has("nvim-0.7"),
+  -- Layouts define sections of the screen to place windows.
+  -- The position can be "left", "right", "top" or "bottom".
+  -- The size specifies the height/width depending on position. It can be an Int
+  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+  -- Elements are the elements shown in the layout (in order).
+  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+  layouts = {
+    {
+      elements = {
+      -- Elements can be strings or table with id and size keys.
+        { id = "scopes", size = 0.25 },
+        "breakpoints",
+        "stacks",
+        "watches",
       },
-      { id = "stacks", size = 0.3 },
-      { id = "breakpoints", size = 0.1 },
+      size = 40, -- 40 columns
+      position = "left",
     },
-    size = 100,
-    position = "left", -- Can be "left", "right", "top", "bottom"
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 0.25, -- 25% of total lines
+      position = "bottom",
+    },
   },
   floating = {
-    max_height = 0.5, -- These can be integers or a float between 0 and 1.
-    max_width = 0.5, -- Floats will be treated as percentage of your screen.
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
     border = "single", -- Border style. Can be "single", "double" or "rounded"
     mappings = {
       close = { "q", "<Esc>" },
     },
   },
   windows = { indent = 1 },
+  render = {
+    max_type_length = nil, -- Can be integer or nil.
+  }
 })
 
 local dap, dapui = require("dap"), require("dapui")
